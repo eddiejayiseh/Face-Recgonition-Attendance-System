949b10354791179ffa0c5518c104b3df611d1c45
Edidiong Iseh <edijacobiseh@gmail.com>
2025-10-24 03:46:37 +0200

    FRAS

---
+++ Test.py
@@ -0,0 +1,23 @@
+import cv2
+
+#Load the pre-trained face detector
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+
+  #Read an image
+img = cv2.imread(r'C:\Users\SAI\Pictures\picture\Image.jpg')
+
+    # Convert the image to grayscale
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    # Detect faces
+faces = face_cascade.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5)
+
+    # Draw rectangles around the faces
+for (x, y, w, h) in faces:
+    cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
+
+    # Display the result
+cv2.imshow('Face Detection', img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
